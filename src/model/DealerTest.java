e88806c78a37c3ccedb1d81c6d7221bdcc315ad4
EEE8D <vhwzjm@gmail.com>
2018-11-28 22:01:12 +0100

    Green: IsDealerWinner_Dealer_AH_9H_Player_AS_10H_ShouldReturnFalse()

--- src/model/DealerTest.java
+++ src/model/DealerTest.java
@@ -16,9 +16,21 @@ class DealerTest {
         a_player.DealCard(new Card(Card.Value.Ace, Card.Color.Spades));
         a_player.DealCard(new Card(Card.Value.Nine, Card.Color.Hearts));
 
-//        int d_score = a_dealer.CalcScore();
-//        int p_score = a_player.CalcScore();
-
         assertTrue(a_dealer.IsDealerWinner(a_player));
+    }
+
+    @Test
+    void IsDealerWinner_Dealer_AH_9H_Player_AS_10H_ShouldReturnFalse(){
+        Dealer a_dealer = new Dealer();
+        Player a_player = new Player();
+
+        a_dealer.DealCard(new Card(Card.Value.Ace, Card.Color.Hearts));
+        a_dealer.DealCard(new Card(Card.Value.Nine, Card.Color.Hearts));
+        a_dealer.ShowHand();
+        a_player.DealCard(new Card(Card.Value.Ace, Card.Color.Spades));
+        a_player.DealCard(new Card(Card.Value.Ten, Card.Color.Hearts));
+        a_player.ShowHand();
+
+        assertFalse(a_dealer.IsDealerWinner(a_player));
     }
 }
