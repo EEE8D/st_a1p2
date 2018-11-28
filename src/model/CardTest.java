727ed0e2fad8d867b74648ea0f87c79c9bccb939
EEE8D <vhwzjm@gmail.com>
2018-11-28 14:47:33 +0100

    more set up AND Junit Practice

---
+++ src/model/CardTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+//import org.junit.platform.commons.logging.Logger;
+//import org.junit.platform.commons.logging.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+//    private static final Logger log = LoggerFactory.getLogger(CardTest.class);
+
+    @Test
+    void JunitPractice(){
+        assertEquals(4, 2+2);
+    }
+
+}
