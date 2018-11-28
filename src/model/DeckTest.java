db0f0dbb742a49d1dc187a18e44bf3fb7e72be3d
EEE8D <vhwzjm@gmail.com>
2018-11-28 16:10:40 +0100

    Red: Deck getCard()

--- src/model/DeckTest.java
+++ src/model/DeckTest.java
@@ -14,4 +14,16 @@ class DeckTest {
         mocked_deck.Shuffle();
         verify(mocked_deck).Shuffle();
     }
+
+    @Test
+    void getCard(){
+        Deck a_deck = new Deck();
+        assertEquals(new Card(Card.Value.Two, Card.Color.Hearts), a_deck.getCard());
+    }
+
+//    @Test
+//    void newDeck_ShouldHas52Cards(){
+//        Deck a_deck = new Deck();
+//        assertTrue(a_deck.m_cards.contains());
+//    }
 }
