9b82c17605826b89e5e8fddc57a4d48a744e9170
EEE8D <vhwzjm@gmail.com>
2018-11-29 22:48:23 +0100

    Green: CollectEvent_p_ShouldReturnChar()

--- src/view/SimpleView.java
+++ src/view/SimpleView.java
@@ -1,8 +1,48 @@
 package view;
 
+import java.io.IOException;
+
 public class SimpleView {
 
-    int getInputChar(){
-        return 'p';
+    int m_input;
+
+    int GetInputChar()  {
+        try {
+            int c = System.in.read();
+            while (c == '\r' || c == '\n') {
+                c = System.in.read();
+            }
+            return c;
+        } catch (java.io.IOException e) {
+            System.out.println("" + e);
+            return 0;
+        }
+    }
+
+    public void collectEvents() {
+        m_input = GetInputChar();
+    }
+
+    public boolean wantsToStartNewGame() {
+        return false;
+//        return m_input == 'p';
+    }
+
+    public boolean wantsToHit() {
+        return false;
+
+//        return m_input == 'h';
+    }
+
+    public boolean wantsToStand() {
+        return false;
+
+//        return m_input == 's';
+    }
+
+    public boolean wantsToQuit() {
+        return false;
+
+//        return m_input == 'q';
     }
 }
--- src/view/SimpleViewTest.java
+++ src/view/SimpleViewTest.java
@@ -1,25 +1,32 @@
 package view;
 
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleViewTest {
 
     //First test input
-    @Test
-    void GetInputChar_p_ShouldReturnChar(){
-        SimpleView a_view = new SimpleView();
-        int actual = a_view.getInputChar();
+    @Test //mock System.in
+    void CollectEvent_p_ShouldReturnChar(){
+        SimpleView a_view = mock(SimpleView.class);
+        when(a_view.GetInputChar()).thenReturn((int)'p');
+
+        int actual = a_view.GetInputChar();
         int expect = 'p';
+
         assertEquals(expect, actual);
     }
 
     @Test
-    void GetInputChar_h_ShouldReturnChar(){
-        SimpleView a_view = new SimpleView();
-        int actual = a_view.getInputChar();
+    void CollectEvent_h_ShouldReturnChar(){
+        SimpleView a_view = mock(SimpleView.class);
+        when(a_view.GetInputChar()).thenReturn((int)'h');
+
+        int actual = a_view.GetInputChar();
         int expect = 'h';
+
         assertEquals(expect, actual);
     }
 
