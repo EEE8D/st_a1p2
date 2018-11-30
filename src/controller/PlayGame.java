54b09af7fd0cfa82390d7268c97555ee12e7c2ab
EEE8D <vhwzjm@gmail.com>
2018-11-30 02:21:53 +0100

    Green: pass

--- src/controller/PlayGame.java
+++ src/controller/PlayGame.java
@@ -13,6 +13,10 @@ public class PlayGame {
         a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
         a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
 
+        if (a_game.IsGameOver()) {
+            a_view.DisplayWinner(a_game.IsDealerWinner());
+        }
+
         return true;
     }
 }
